ddc16aeca9455bb004a4d793577289f11ec6ce11
sudais605 <durranidon92@gmail.com>
2026-09-15 08:10:06 -0700

    Implement basic plugin structure for OblivionRelics

--- src/main/java/me/oblivion/relics/OblivionRelics.java
+++ src/main/java/me/oblivion/relics/OblivionRelics.java
@@ -1,1 +1,16 @@
+package me.oblivion.relics;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OblivionRelics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("OblivionRelics has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("OblivionRelics has been disabled!");
+    }
+}
