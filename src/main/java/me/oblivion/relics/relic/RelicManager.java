d35731c3aa4062c764c079736046665cfdcd0f91
sudais605 <durranidon92@gmail.com>
2026-09-16 08:51:33 -0700

    Update RelicManager.java

--- src/main/java/me/oblivion/relics/relic/RelicManager.java
+++ src/main/java/me/oblivion/relics/relic/RelicManager.java
@@ -2,6 +2,7 @@ package me.oblivion.relics.relic;
 
 import me.oblivion.relics.data.PlayerDataManager;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 import java.util.UUID;
@@ -9,27 +10,40 @@ import java.util.UUID;
 public class RelicManager {
 
     private final PlayerDataManager playerDataManager;
+    private final RelicItem relicItem;
     private final Random random = new Random();
 
-    public RelicManager(PlayerDataManager playerDataManager) {
+    public RelicManager(
+            PlayerDataManager playerDataManager,
+            RelicItem relicItem
+    ) {
         this.playerDataManager = playerDataManager;
+        this.relicItem = relicItem;
     }
 
     public RelicType getRelic(UUID uuid) {
-        String relicName = playerDataManager.getRelic(uuid);
+
+        String relicName =
+                playerDataManager.getRelic(uuid);
 
         if (relicName == null || relicName.isBlank()) {
             return null;
         }
 
         try {
-            return RelicType.valueOf(relicName.toUpperCase());
+            return RelicType.valueOf(
+                    relicName.toUpperCase()
+            );
         } catch (IllegalArgumentException exception) {
             return null;
         }
     }
 
-    public void setRelic(UUID uuid, RelicType relic) {
+    public void setRelic(
+            UUID uuid,
+            RelicType relic
+    ) {
+
         if (relic == null) {
             playerDataManager.setRelic(uuid, "");
             return;
@@ -42,15 +56,48 @@ public class RelicManager {
     }
 
     public RelicType giveRandomRelic(UUID uuid) {
+
         RelicType[] relics = RelicType.values();
 
-        RelicType selected = relics[
-                random.nextInt(relics.length)
-        ];
+        RelicType selected =
+                relics[random.nextInt(relics.length)];
 
         setRelic(uuid, selected);
 
         return selected;
+    }
+
+    public ItemStack createRelicItem(UUID uuid) {
+
+        RelicType relic =
+                getRelic(uuid);
+
+        if (relic == null) {
+            return null;
+        }
+
+        return relicItem.create(relic);
+    }
+
+    public ItemStack createRelicItem(
+            RelicType relic
+    ) {
+
+        if (relic == null) {
+            return null;
+        }
+
+        return relicItem.create(relic);
+    }
+
+    public boolean isRelicItem(ItemStack item) {
+        return relicItem.isRelic(item);
+    }
+
+    public RelicType getRelicFromItem(
+            ItemStack item
+    ) {
+        return relicItem.getRelicType(item);
     }
 
     public boolean hasRelic(UUID uuid) {
@@ -58,6 +105,8 @@ public class RelicManager {
     }
 
     public boolean hasRelic(Player player) {
-        return hasRelic(player.getUniqueId());
+        return hasRelic(
+                player.getUniqueId()
+        );
     }
 }
