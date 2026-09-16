2503f5f1b7b76053f60f7f4744517a11e547afe2
sudais605 <durranidon92@gmail.com>
2026-09-16 08:50:53 -0700

    Update OblivionRelics.java

--- src/main/java/me/oblivion/relics/OblivionRelics.java
+++ src/main/java/me/oblivion/relics/OblivionRelics.java
@@ -1,96 +1,121 @@
-package me.oblivion.relics.relic;
+package me.oblivion.relics;
 
+import me.oblivion.relics.command.TrustCommand;
+import me.oblivion.relics.command.WithdrawCommand;
 import me.oblivion.relics.data.PlayerDataManager;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import me.oblivion.relics.energy.EchoFlask;
+import me.oblivion.relics.energy.EnergyManager;
+import me.oblivion.relics.listener.FlaskListener;
+import me.oblivion.relics.relic.RelicItem;
+import me.oblivion.relics.relic.RelicManager;
+import me.oblivion.relics.trust.TrustManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Random;
-import java.util.UUID;
+public final class OblivionRelics extends JavaPlugin {
 
-public class RelicManager {
+    private PlayerDataManager playerDataManager;
+    private EnergyManager energyManager;
+    private EchoFlask echoFlask;
+    private TrustManager trustManager;
+    private RelicItem relicItem;
+    private RelicManager relicManager;
 
-    private final PlayerDataManager playerDataManager;
-    private final RelicItem relicItem;
-    private final Random random = new Random();
+    @Override
+    public void onEnable() {
 
-    public RelicManager(
-            PlayerDataManager playerDataManager,
-            RelicItem relicItem
-    ) {
-        this.playerDataManager = playerDataManager;
-        this.relicItem = relicItem;
+        playerDataManager = new PlayerDataManager(this);
+
+        energyManager = new EnergyManager(
+                playerDataManager
+        );
+
+        echoFlask = new EchoFlask(this);
+
+        trustManager = new TrustManager(
+                playerDataManager
+        );
+
+        relicItem = new RelicItem(this);
+
+        relicManager = new RelicManager(
+                playerDataManager,
+                relicItem
+        );
+
+        getCommand("withdraw").setExecutor(
+                new WithdrawCommand(
+                        energyManager,
+                        echoFlask
+                )
+        );
+
+        getCommand("trust").setExecutor(
+                new TrustCommand(
+                        trustManager
+                )
+        );
+
+        getServer().getPluginManager().registerEvents(
+                new FlaskListener(
+                        echoFlask,
+                        energyManager
+                ),
+                this
+        );
+
+        getLogger().info(
+                "OblivionRelics has been enabled!"
+        );
+
+        getLogger().info(
+                "Player data system loaded."
+        );
+
+        getLogger().info(
+                "Energy system loaded."
+        );
+
+        getLogger().info(
+                "Echo Flask system loaded."
+        );
+
+        getLogger().info(
+                "Trust system loaded."
+        );
+
+        getLogger().info(
+                "Relic system loaded."
+        );
     }
 
-    public RelicType getRelic(UUID uuid) {
-
-        String relicName = playerDataManager.getRelic(uuid);
-
-        if (relicName == null || relicName.isBlank()) {
-            return null;
-        }
-
-        try {
-            return RelicType.valueOf(relicName.toUpperCase());
-        } catch (IllegalArgumentException exception) {
-            return null;
-        }
+    @Override
+    public void onDisable() {
+        getLogger().info(
+                "OblivionRelics has been disabled!"
+        );
     }
 
-    public void setRelic(UUID uuid, RelicType relic) {
-
-        if (relic == null) {
-            playerDataManager.setRelic(uuid, "");
-            return;
-        }
-
-        playerDataManager.setRelic(uuid, relic.name());
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
     }
 
-    public RelicType giveRandomRelic(UUID uuid) {
-
-        RelicType[] relics = RelicType.values();
-
-        RelicType selected =
-                relics[random.nextInt(relics.length)];
-
-        setRelic(uuid, selected);
-
-        return selected;
+    public EnergyManager getEnergyManager() {
+        return energyManager;
     }
 
-    public ItemStack createRelicItem(UUID uuid) {
-
-        RelicType relic = getRelic(uuid);
-
-        if (relic == null) {
-            return null;
-        }
-
-        return relicItem.create(relic);
+    public EchoFlask getEchoFlask() {
+        return echoFlask;
     }
 
-    public ItemStack createRelicItem(RelicType relic) {
-
-        if (relic == null) {
-            return null;
-        }
-
-        return relicItem.create(relic);
+    public TrustManager getTrustManager() {
+        return trustManager;
     }
 
-    public boolean isRelicItem(ItemStack item) {
-        return relicItem.isRelic(item);
+    public RelicItem getRelicItem() {
+        return relicItem;
     }
 
-    public RelicType getRelicFromItem(ItemStack item) {
-        return relicItem.getRelicType(item);
-    }
-
-    public boolean hasRelic(UUID uuid) {
-        return getRelic(uuid) != null;
-    }
-
-    public boolean hasRelic(Player player) {
-        return hasRelic(player.getUniqueId());
+    public RelicManager getRelicManager() {
+        return relicManager;
     }
 }
