c2db425453dbb64d8583bdd288c33bb2944eb93c
sudais605 <durranidon92@gmail.com>
2026-09-16 04:09:42 -0700

    Update OblivionRelics.java

--- src/main/java/me/oblivion/relics/OblivionRelics.java
+++ src/main/java/me/oblivion/relics/OblivionRelics.java
@@ -1,29 +1,45 @@
 package me.oblivion.relics;
 
+import me.oblivion.relics.command.WithdrawCommand;
 import me.oblivion.relics.data.PlayerDataManager;
+import me.oblivion.relics.energy.EchoFlask;
 import me.oblivion.relics.energy.EnergyManager;
+import me.oblivion.relics.listener.FlaskListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OblivionRelics extends JavaPlugin {
 
     private PlayerDataManager playerDataManager;
     private EnergyManager energyManager;
+    private EchoFlask echoFlask;
 
     @Override
     public void onEnable() {
+
         saveDefaultConfig();
 
         playerDataManager = new PlayerDataManager(this);
         energyManager = new EnergyManager(playerDataManager);
+        echoFlask = new EchoFlask(this);
+
+        getCommand("withdraw").setExecutor(
+                new WithdrawCommand(energyManager, echoFlask)
+        );
+
+        getServer().getPluginManager().registerEvents(
+                new FlaskListener(echoFlask, energyManager),
+                this
+        );
 
         getLogger().info("OblivionRelics has been enabled!");
         getLogger().info("Player data system loaded.");
         getLogger().info("Energy system loaded.");
+        getLogger().info("Echo Flask system loaded.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("OblivionRelics has been disabled.");
+        getLogger().info("OblivionRelics has been disabled!");
     }
 
     public PlayerDataManager getPlayerDataManager() {
@@ -32,5 +48,9 @@ public final class OblivionRelics extends JavaPlugin {
 
     public EnergyManager getEnergyManager() {
         return energyManager;
+    }
+
+    public EchoFlask getEchoFlask() {
+        return echoFlask;
     }
 }
