a4ba1a85f18abf72145d06b211839d004e607f63
sudais605 <durranidon92@gmail.com>
2026-09-16 04:05:40 -0700

    Add EnergyManager to OblivionRelics plugin

--- src/main/java/me/oblivion/relics/OblivionRelics.java
+++ src/main/java/me/oblivion/relics/OblivionRelics.java
@@ -1,28 +1,36 @@
 package me.oblivion.relics;
 
 import me.oblivion.relics.data.PlayerDataManager;
+import me.oblivion.relics.energy.EnergyManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OblivionRelics extends JavaPlugin {
 
     private PlayerDataManager playerDataManager;
+    private EnergyManager energyManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
         playerDataManager = new PlayerDataManager(this);
+        energyManager = new EnergyManager(playerDataManager);
 
         getLogger().info("OblivionRelics has been enabled!");
         getLogger().info("Player data system loaded.");
+        getLogger().info("Energy system loaded.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("OblivionRelics has been disabled!");
+        getLogger().info("OblivionRelics has been disabled.");
     }
 
     public PlayerDataManager getPlayerDataManager() {
         return playerDataManager;
+    }
+
+    public EnergyManager getEnergyManager() {
+        return energyManager;
     }
 }
