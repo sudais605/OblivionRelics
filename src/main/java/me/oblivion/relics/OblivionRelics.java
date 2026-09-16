910615f5f47264efd3322878de5a57435486c3d9
sudais605 <durranidon92@gmail.com>
2026-09-16 04:02:53 -0700

    Refactor OblivionRelics to manage player data
    Removed command handling and added PlayerDataManager.

--- src/main/java/me/oblivion/relics/OblivionRelics.java
+++ src/main/java/me/oblivion/relics/OblivionRelics.java
@@ -1,14 +1,20 @@
 package me.oblivion.relics;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import me.oblivion.relics.data.PlayerDataManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OblivionRelics extends JavaPlugin {
 
+    private PlayerDataManager playerDataManager;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        playerDataManager = new PlayerDataManager(this);
+
         getLogger().info("OblivionRelics has been enabled!");
+        getLogger().info("Player data system loaded.");
     }
 
     @Override
@@ -16,18 +22,7 @@ public final class OblivionRelics extends JavaPlugin {
         getLogger().info("OblivionRelics has been disabled!");
     }
 
-    @Override
-    public boolean onCommand(
-            CommandSender sender,
-            Command command,
-            String label,
-            String[] args
-    ) {
-        if (command.getName().equalsIgnoreCase("relictest")) {
-            sender.sendMessage("§bOblivionRelics §7test successful!");
-            return true;
-        }
-
-        return false;
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
     }
 }
