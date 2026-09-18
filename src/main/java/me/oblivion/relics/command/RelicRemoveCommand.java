d611e7896105a63fec2fb2e074c68bf2976b6625
sudais605 <durranidon92@gmail.com>
2026-09-18 05:52:07 -0700

    Update RelicRemoveCommand.java

--- src/main/java/me/oblivion/relics/command/RelicRemoveCommand.java
+++ src/main/java/me/oblivion/relics/command/RelicRemoveCommand.java
@@ -1,5 +1,6 @@
 package me.oblivion.relics.command;
 
+import me.oblivion.relics.OblivionRelics;
 import me.oblivion.relics.relic.RelicManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -9,57 +10,138 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-public class RelicRemoveCommand implements CommandExecutor {
+public class RelicRemoveCommand
+        implements CommandExecutor {
 
     private final RelicManager relicManager;
 
-    public RelicRemoveCommand(RelicManager relicManager) {
+    public RelicRemoveCommand(
+            RelicManager relicManager
+    ) {
         this.relicManager = relicManager;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args
+    ) {
+
         if (!sender.isOp()) {
-            sender.sendMessage(ChatColor.RED + "Only OP players can use this command.");
+
+            sender.sendMessage(
+                    ChatColor.RED
+                            + "Only OP players can use this command."
+            );
+
             return true;
         }
 
         Player target;
 
         if (args.length == 0) {
+
             if (!(sender instanceof Player player)) {
-                sender.sendMessage(ChatColor.RED + "Usage: /relicremove <player>");
+
+                sender.sendMessage(
+                        ChatColor.RED
+                                + "Usage: /relicremove <player>"
+                );
+
                 return true;
             }
+
             target = player;
+
         } else {
-            target = Bukkit.getPlayerExact(args[0]);
+
+            target =
+                    Bukkit.getPlayerExact(
+                            args[0]
+                    );
+
             if (target == null) {
-                sender.sendMessage(ChatColor.RED + "That player is not online.");
+
+                sender.sendMessage(
+                        ChatColor.RED
+                                + "That player is not online."
+                );
+
                 return true;
             }
         }
 
         if (!relicManager.hasRelic(target)) {
-            sender.sendMessage(ChatColor.RED + target.getName() + " does not have a Relic.");
+
+            sender.sendMessage(
+                    ChatColor.RED
+                            + target.getName()
+                            + " does not have a Relic."
+            );
+
             return true;
         }
 
-        String oldRelic = relicManager.getRelic(target.getUniqueId()).getDisplayName();
-        relicManager.setRelic(target.getUniqueId(), null);
+        var relic =
+                relicManager.getRelic(
+                        target.getUniqueId()
+                );
 
-        for (int slot = 0; slot < target.getInventory().getSize(); slot++) {
-            ItemStack item = target.getInventory().getItem(slot);
-            if (relicManager.isRelicItem(item)) {
-                target.getInventory().setItem(slot, null);
+        String oldRelic =
+                relic.getDisplayName();
+
+        relicManager.setRelic(
+                target.getUniqueId(),
+                null
+        );
+
+        if (sender instanceof Player) {
+
+            OblivionRelics plugin =
+                    OblivionRelics.getPlugin(
+                            OblivionRelics.class
+                    );
+
+            if (plugin != null) {
+
+                plugin.getRelicAbilityEngine()
+                        .clearPlayerState(
+                                target.getUniqueId()
+                        );
             }
         }
 
-        sender.sendMessage(ChatColor.GREEN + "Removed " + oldRelic + " from " + target.getName() + ".");
+        for (int slot = 0;
+             slot < target.getInventory().getSize();
+             slot++) {
 
-        if (!target.equals(sender)) {
-            target.sendMessage(ChatColor.RED + "Your Relic has been removed.");
+            ItemStack item =
+                    target.getInventory()
+                            .getItem(slot);
+
+            if (item != null
+                    && relicManager.isRelicItem(item)) {
+
+                target.getInventory()
+                        .setItem(slot, null);
+            }
         }
+
+        sender.sendMessage(
+                ChatColor.GREEN
+                        + "Removed "
+                        + oldRelic
+                        + " from "
+                        + target.getName()
+                        + "."
+        );
+
+        target.sendMessage(
+                ChatColor.RED
+                        + "Your Relic has been removed."
+        );
 
         return true;
     }
