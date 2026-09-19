ae4a01ca4870f444df7831a35b4467ebb17b606b
sudais605 <durranidon92@gmail.com>
2026-09-19 06:25:15 -0700

    Refactor death message listener for readability

--- src/main/java/me/oblivion/relics/listener/AbilityDeathMessageListener.java
+++ src/main/java/me/oblivion/relics/listener/AbilityDeathMessageListener.java
@@ -19,30 +19,21 @@ public class AbilityDeathMessageListener implements Listener {
     }
 
     @EventHandler
-    public void onDeath(
-            PlayerDeathEvent event
-    ) {
+    public void onDeath(PlayerDeathEvent event) {
 
-        Player victim =
-                event.getEntity();
+        Player victim = event.getEntity();
 
         if (!(victim.getLastDamageCause()
                 instanceof EntityDamageByEntityEvent damageEvent)) {
-
             return;
         }
 
         if (!(damageEvent.getDamager()
                 instanceof Player killer)) {
-
             return;
         }
 
-        if (engine.isTrusted(
-                killer,
-                victim
-        )) {
-
+        if (engine.isTrusted(killer, victim)) {
             return;
         }
 
@@ -56,58 +47,158 @@ public class AbilityDeathMessageListener implements Listener {
         String ability =
                 record.getAbilityName();
 
-        event.setDeathMessage(
+        String message =
                 ChatColor.GRAY
                         + victim.getName()
-                        + getPhrase(ability)
+                        + getDeathPhrase(ability)
                         + killer.getName()
                         + "'s "
                         + ability
-                        + "."
-        );
+                        + ".";
+
+        event.setDeathMessage(message);
     }
 
-    private String getPhrase(
+    private String getDeathPhrase(
             String ability
     ) {
 
         return switch (ability) {
 
+            case "Rift Pull" ->
+                    " was pulled apart by ";
+
             case "Rift Dash" ->
                     " was torn through by ";
+
+            case "Rift Break" ->
+                    " was shattered by ";
+
+            case "Gravity Well" ->
+                    " was dragged down by ";
+
+            case "Gravity Lift" ->
+                    " was crushed by ";
+
+            case "Gravity Crush" ->
+                    " was crushed by ";
+
+            case "Void Step" ->
+                    " was consumed by ";
+
+            case "Void Snare" ->
+                    " was trapped by ";
+
+            case "Void Collapse" ->
+                    " was erased by ";
 
             case "Storm Pierce" ->
                     " was struck down by ";
 
+            case "Tempest Dash" ->
+                    " was torn apart by ";
+
+            case "Storm Cage" ->
+                    " was overwhelmed by ";
+
             case "Frost Lance" ->
                     " was frozen by ";
+
+            case "Ice Prison" ->
+                    " was trapped in ice by ";
+
+            case "Frozen Ground" ->
+                    " was frozen by ";
+
+            case "Flame Burst" ->
+                    " was burned by ";
 
             case "Ember Shot" ->
                     " was burned by ";
 
+            case "Inferno Ring" ->
+                    " was engulfed by ";
+
+            case "Shadow Blink" ->
+                    " was caught by ";
+
             case "Shadow Pierce" ->
                     " was pierced by ";
+
+            case "Blackout" ->
+                    " was swallowed by darkness from ";
+
+            case "Time Acceleration" ->
+                    " was overwhelmed by ";
+
+            case "Rewind" ->
+                    " was outplayed by ";
 
             case "Time Lock" ->
                     " was stopped by ";
 
+            case "Phantom Rush" ->
+                    " was struck by ";
+
+            case "Phase Shift" ->
+                    " was overwhelmed by ";
+
             case "Phantom Strike" ->
                     " was struck by ";
+
+            case "Soul Pull" ->
+                    " had their soul pulled by ";
+
+            case "Soul Mark" ->
+                    " was marked by ";
 
             case "Soul Burst" ->
                     " had their soul shattered by ";
 
+            case "Chaos Bolt" ->
+                    " was destroyed by ";
+
+            case "Chaos Swap" ->
+                    " was outplayed by ";
+
+            case "Chaos Field" ->
+                    " was overwhelmed by ";
+
             case "Celestial Spear" ->
                     " was pierced by ";
+
+            case "Celestial Guard" ->
+                    " was overwhelmed by ";
+
+            case "Starfall" ->
+                    " was crushed beneath ";
 
             case "Aegis Bash" ->
                     " was crushed by ";
 
+            case "AutoCrit" ->
+                    " was critically struck by ";
+
+            case "Aegis Break" ->
+                    " was shattered by ";
+
             case "Force Push" ->
                     " was blasted away by ";
 
-            case "Arcane Bolt" ->
+            case "Force Launch" ->
+                    " was launched by ";
+
+            case "Force Wave" ->
                     " was hit by ";
+
+            case "Arcane Bolt" ->
+                    " was struck by ";
+
+            case "Arcane Rift" ->
+                    " was overwhelmed by ";
+
+            case "Arcane Burst" ->
+                    " was destroyed by ";
 
             default ->
                     " was defeated by ";
